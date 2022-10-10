b44e2845a5ea0705544b7b3b398cc00c0d2444e3
Gabriel Klavans <gabeklav@fastmail.com>
2022-10-10 00:49:33 -0400

    Initial Commit

---
+++ T-Party/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  T-Party
+//
+//  Created by Gabriel Klavans on 10/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ T-Party/T_PartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  T_PartyApp.swift
+//  T-Party
+//
+//  Created by Gabriel Klavans on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct T_PartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
