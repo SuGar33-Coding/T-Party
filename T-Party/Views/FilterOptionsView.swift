98719041f023f40c0b8d3730a86c3929116837fd
Emily Mallaber <emilymallaber@Emilys-MacBook-Air.local>
2023-02-05 17:08:00 -0500

    filter button on map view, all the changes to Stop/Stops class/struct/etc

---
+++ T-Party/Views/FilterOptionsView.swift
@@ -0,0 +1,20 @@
+//
+//  FilterOptionsView.swift
+//  T-Party
+//
+//  Created by Emily Mallaber on 2/5/23.
+//
+
+import SwiftUI
+
+struct FilterOptionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FilterOptionsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FilterOptionsView()
+    }
+}
