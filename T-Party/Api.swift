ad9ab6c15214826b16ee578a380aeb4526a2ec46
gabe <gabeklav@fastmail.com>
2024-01-15 18:11:32 -0500

    add melinoe api endpoint

--- T-Party/Api.swift
+++ T-Party/Api.swift
@@ -12,6 +12,8 @@ private enum domains: String {
     case caprover = "https://t-server.cap.dabe.tech"
     case local = "http://localhost:3000"
     case macaria = "http://macaria.local:3000"
+    case windows = "http://192.168.88.128:3000"
+    case melinoe = "http://melinoe.local:3000"
 }
 
 let serverDomain: String = domains.caprover.rawValue
